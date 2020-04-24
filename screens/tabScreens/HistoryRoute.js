9ccdc6fe7f25c283d63fdf98fd2e12bba728f0ad
Ushio Shinohara <louishinohara@gmail.com>
2020-04-23 22:42:14 -0400

    Update HistoryRoute.js

--- screens/tabScreens/HistoryRoute.js
+++ screens/tabScreens/HistoryRoute.js
@@ -21,15 +21,15 @@ constructor(props) {
     super(props);
     this.state = {
       data: [
-        {id:1, color:"#23b526", icon:"https://i.imgur.com/1R82UKp.png", date: "Monday, March 5, 2020", tags:['Mood1', 'Mood2', 'Mood3','Mood4'], message: 'Ate some icecream'},
-        {id:2, color:"#FF3714", icon:"https://i.imgur.com/r1uqWbZ.png", date: "Sunday, March 4, 2020", tags:['Mood1', ],message: 'Got corona today'},
-        {id:3, color:"#fd7c1a", icon:"https://i.imgur.com/N6d2HMQ.png", date: "Saturday, March 3, 2020", tags:['Mood1', 'Mood2', 'Mood3',],message: 'Hurt my knee'}, 
-        {id:4, color:"#fed41c", icon:"https://i.imgur.com/XD8gBGr.png", date: "Friday, March 2, 2020", tags:['Mood1', 'Mood2',],message: 'Forgot to do HW'}, 
-        {id:5, color:"#a1df39", icon:"https://i.imgur.com/UTUIdBX.png", date: "Thursday, March 1, 2020", tags:['Mood1', 'Mood2', 'Mood3','Mood4'],message: 'Did good on exam'}, 
-        {id:6, color:"#fed41c", icon:"https://i.imgur.com/XD8gBGr.png", date: "Wednesday, February 29, 2020", tags:['Mood1', 'Mood2', 'Mood3', 'Mood4'],message: 'Ate some icecream'}, 
-        {id:7, color:"#23b526", icon:"https://i.imgur.com/1R82UKp.png", date: "Tuesday, February 28, 2020", tags:['Mood1', 'Mood2', 'Mood3'],message: 'Ate some icecream'},
-        {id:8, color:"#FF3714", icon:"https://i.imgur.com/r1uqWbZ.png", date: "Monday, February 27, 2020", tags:['Mood1', 'Mood2', 'Mood3'],message: 'Ate some icecream'},
-        {id:9, color:"#23b526", icon:"https://i.imgur.com/1R82UKp.png", date: "Sunday, February 26, 2020", tags:['Mood1', 'Mood2', 'Mood3'],message: 'Ate some icecream'},
+        {id:1, color:"#23b526", icon:"https://i.imgur.com/1R82UKp.png", date: "3/5/2020", tags:['Mood1', 'Mood2', 'Mood3','Mood4'], message: 'Ate some icecream'},
+        {id:2, color:"#FF3714", icon:"https://i.imgur.com/r1uqWbZ.png", date: "3/4/2020", tags:['Mood1', ],message: 'Got corona today'},
+        {id:3, color:"#fd7c1a", icon:"https://i.imgur.com/N6d2HMQ.png", date: "3/3/2020", tags:['Mood1', 'Mood2', 'Mood3',],message: 'Hurt my knee'}, 
+        {id:4, color:"#fed41c", icon:"https://i.imgur.com/XD8gBGr.png", date: "3/2/2020", tags:['Mood1', 'Mood2',],message: 'Forgot to do HW'}, 
+        {id:5, color:"#a1df39", icon:"https://i.imgur.com/UTUIdBX.png", date: "3/1/2020", tags:['Mood1', 'Mood2', 'Mood3','Mood4'],message: 'Did good on exam'}, 
+        {id:6, color:"#fed41c", icon:"https://i.imgur.com/XD8gBGr.png", date: "2/29/2020", tags:['Mood1', 'Mood2', 'Mood3', 'Mood4'],message: 'Ate some icecream'}, 
+        {id:7, color:"#23b526", icon:"https://i.imgur.com/1R82UKp.png", date: "2/28/2020", tags:['Mood1', 'Mood2', 'Mood3'],message: 'Ate some icecream'},
+        {id:8, color:"#FF3714", icon:"https://i.imgur.com/r1uqWbZ.png", date: "2/27/2020", tags:['Mood1', 'Mood2', 'Mood3'],message: 'Ate some icecream'},
+        {id:9, color:"#23b526", icon:"https://i.imgur.com/1R82UKp.png", date: "2/26/2020", tags:['Mood1', 'Mood2', 'Mood3'],message: 'Ate some icecream'},
       ],
     };
   }
@@ -137,11 +137,15 @@ const styles = StyleSheet.create({
     borderRadius:30,
   },
   date:{
-    fontSize:18,
-    fontWeight: '400',
-    marginLeft:10,
+    fontSize: 17,
+    fontWeight: '500',
+    marginLeft: 10,
     alignSelf: 'center',
-    color: 'grey'
+    color: '#eff0f1',
+    position: 'absolute',
+    height: 120,
+    paddingLeft:155,
+
   },
   btnColor: {
     padding:10,
