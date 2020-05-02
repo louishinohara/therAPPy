f72a7f45973cbc69d23c909333ab8b12ef5036ef
Ushio Shinohara <louishinohara@gmail.com>
2020-05-02 15:00:28 -0400

    Update 2ndchart.js

--- screens/tabScreens/visualizationGraphs/2ndchart.js
+++ screens/tabScreens/visualizationGraphs/2ndchart.js
@@ -4,6 +4,8 @@ import { BarChart, Grid,XAxis } from 'react-native-svg-charts'
 import * as scale from 'd3-scale'
 import {getData} from '../getData'
 
+var wholeData = []
+
     class AnotherScreen extends React.Component {
            constructor() {
             super()
@@ -11,36 +13,39 @@ import {getData} from '../getData'
                 wholeData: []
             }
         }
-
-
-
-        toggle = () => {
+        toggle = (data) => {
             var a = [1,2,3]
-            console.log(a)
+            console.log(data)
         };
     render() {
 
         const db = firebase.firestore();
         var UID = firebase.auth().currentUser.uid;
-        var wholeData = []
+        
         db.collection('surveyData').where("userID", "==", UID ).get()
         .then(snapshot => {
         snapshot.forEach(doc => {
-            wholeData.push(doc.data())
-            this.toggle
+            // console.log(wholeData)
+            var data = doc.data()
+            wholeData.push(data)
+            // console.log(wholeData)
+            
+            // console.log(data.academicAvg)
+            
         });
-        
+
         })
         .catch(error => {
         console.log('Error!', error);
         })
-
+        
         var data = [3, 1, 4, 9, 1, 1];
+        console.log('wholeData')
         return (
             
             <View style={{ height: 200, padding: 20 }}>
 
-            <Button title="Select Month" onPress={this.toggle} />
+            <Button title="Select Month" onPress={this.toggle(wholeData)} />
           
             </View>
 
